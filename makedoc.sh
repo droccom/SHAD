c82a2c4ab1b7064a1f0fb35412d0723cf9d5c26c
Maurizio Drocco <maurizio.drocco@pnnl.gov>
2018-08-27 17:34:48 -0400

    [pnnl/SHAD#100] tentative fix

--- makedoc.sh
+++ makedoc.sh
@@ -78,8 +78,12 @@ doxygen $DOXYFILE 2>&1 | tee doxygen.log
 # Only upload if Doxygen successfully created the documentation.
 # Check this by verifying that the html directory and the file html/index.html
 # both exist. This is a good indication that Doxygen did it's work.
-if [ -f "index.html" ]; then
+if [ -d "html" ] && [ -f "html/index.html" ]; then
     echo 'Uploading documentation to the gh-pages branch...'
+
+    # Move everything to the top level
+    mv html/* .
+
     # Add everything in this directory (the Doxygen code documentation) to the
     # gh-pages branch.
     # GitHub is smart enough to know which files have changed and which files have
